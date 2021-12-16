14da21313229e5e6c67fe2fcc7698d38ef2fcbaa
Marina Belova <83077065+Salirsky@users.noreply.github.com>
2021-12-16 12:54:46 +0300

    Connect the slider part 3

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const swiper = new Swiper('.swiper', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
     },
   
 
